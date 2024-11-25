a54ca4b2da1fc86e7a6863b29cedb70dede51b90
Hbitz <53080862+Hbitz@users.noreply.github.com>
2024-11-25 10:38:45 +0100

    Created classes Character and Attack

---
+++ BattleBarbarians/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleBarbarians
+{
+    internal class Attack
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int ManaCost { get; set; }
+        public string Description { get; set; }
+
+        public Attack(string name, int damage, int manaCost, string description)
+        {
+            Name = name;
+            Damage = damage;
+            ManaCost = manaCost;
+            Description = description;
+        }
+    }
+}
