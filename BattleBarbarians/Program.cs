55cc4957d5afa678f41a5165bf9acdefe491caae
Hbitz <53080862+Hbitz@users.noreply.github.com>
2024-11-25 10:00:00 +0100

    Add project files.

---
+++ BattleBarbarians/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleBarbarians
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
