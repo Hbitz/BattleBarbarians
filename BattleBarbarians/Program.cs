000c928c95a4ecc95cf87d8aa15f63b433cb0e26
Hbitz <53080862+Hbitz@users.noreply.github.com>
2024-11-29 14:52:27 +0100

    Small todos added

--- BattleBarbarians/Program.cs
+++ BattleBarbarians/Program.cs
@@ -4,9 +4,14 @@
     {
         // Todo
         // Some mental notes to not forget
+        // Generic: Either Convert Character, or expand rewards and introduce an inventory system.
+        // Implement logic for attack power modifier to allow a more diverse set of powerups
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(Console.WindowWidth);
+            Console.WriteLine(Console.WindowHeight);
+            Console.SetWindowSize(120, 40);
 
             GameController gameController = new GameController();
             gameController.StartGame();
